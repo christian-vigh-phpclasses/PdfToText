65acf5b0dca74adf07f9a208b0471c7a90085205
christian-vigh <christian.vigh@orange.fr>
2017-04-26 15:51:35 +0200

    Added an example for form data extraction

---
+++ examples/formdata-extraction/README.md
@@ -0,0 +1,3 @@
+This example shows how to extract form data, either by using an XML template or no XML template at all.
+
+The supplied PDF file, *sample.pdf*, is a form that have been filled and saved.
---
+++ examples/formdata-extraction/example.php
@@ -0,0 +1,21 @@
+<?php
+	include ( '../../PdfToText.phpclass' ) ;
+
+	if  ( php_sapi_name ( )  !=  'cli' )
+		echo ( "<pre>" ) ;
+
+
+	echo "Form data extraction using an XML definition file (sample.pdf) :\n" ;
+	$pdf	=  new PdfToText ( 'sample.pdf' ) ;
+
+	if  ( $pdf -> HasFormData ( ) )
+		var_dump ( $pdf -> GetFormData ( 'sample.xml' ) ) ;
+
+	echo "\n" ;
+
+	echo "Form data extraction WITHOUT using an XML definition file :\n" ;
+	$pdf	=  new PdfToText ( 'sample.pdf' ) ;
+
+	if  ( $pdf -> HasFormData ( ) )
+		var_dump ( $pdf -> GetFormData ( ) ) ;
+
